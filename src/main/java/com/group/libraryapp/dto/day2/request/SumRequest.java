a6302c46ac50f015c72f96f5a38e34f0a4f8e16e
do-park <doheepa@kakao.com>
2024-05-01 20:10:23 +0900

    day2 - 과제 3

---
+++ src/main/java/com/group/libraryapp/dto/day2/request/SumRequest.java
@@ -0,0 +1,19 @@
+package com.group.libraryapp.dto.day2.request;
+
+import java.util.List;
+
+public class SumRequest {
+    private List<Integer> numbers;
+
+    public SumRequest() {
+
+    }
+
+    public SumRequest(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
