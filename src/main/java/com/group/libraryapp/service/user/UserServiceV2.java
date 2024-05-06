8c6d7faf4327613b8b6615a34b1b9bc133957074
do-park <doheepa@kakao.com>
2024-05-06 20:51:11 +0900

    28. 트랜잭션 적용과 영속성 컨텍스트
    IOException과 같은 Checked Exception은 롤백이 일어나지 않는다
    
    영속성 컨텍스트
    테이블과 매핑된 Entity 객체를 관리/보관하는 역할
    스프링에서는 트랜잭션을 사용하면 영속성 컨텍스트가 생겨나고, 트랜잭션이 종료되면 영속성 컨텍스트가 종료된다
    특수 능력
    1. 변경 감지(Dirty Check) 영속성 컨텍스트 안에서 불러와진 Entity는 명시적으로 save하지 않더라도, 변경을 감지해 자동으로 저장된다 (<- 트랜잭션)
    2. 쓰기 지연: DB의 INSERT/UPDATE/DELETE SQL을 바로 날리는 것이 아니라, 트랜잭션이 commit될 때 한 번에 날린다
    3. 1차 캐싱: ID를 기준으로 Entity를 기억한다. 이렇게 캐싱된 객체는 완전히 동일하다
    4. ...다음 이 시간에...

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,16 +19,19 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         userRepository.save(new User(request.getName(), request.getAge()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.getId()).orElseThrow(IllegalAccessError::new);
 
@@ -35,6 +39,7 @@ public class UserServiceV2 {
         userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name);
         if (user == null) {
