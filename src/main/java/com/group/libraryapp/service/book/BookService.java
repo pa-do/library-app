2d2d5a396761dcff4c3358aabc814c0859517125
do-park <doheepa@kakao.com>
2024-05-12 18:31:28 +0900

    33. 조금 더 객체지향적으로 개발할 수 있을까?
    @ManyToOne (N:1)
    나는 다수, 너는 하나
    유저 대출 기록 입장에서 대출 기록은 여러 개인데 대출 기록을 소유하고 있는 건 한 명
    (<-> @OneToMany)
    
    연관관계의 주인
    Table을 보았을 때 누가 관계의 주도권을 가지고 있는가
    연관관계의 주인이 아닌 쪽에 mappedBy 옵션을 달아 주어야 한다
    연관관계의 주인의 값이 설정되어야만 진정한 데이터가 저장된다

--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -44,7 +44,7 @@ public class BookService {
         User user = userRepository.findByName(request.getUserName())
                 .orElseThrow(IllegalArgumentException::new);
 
-        userLoanHistoryRepository.save(new UserLoanHistory(user.getId(), book.getName()));
+        userLoanHistoryRepository.save(new UserLoanHistory(user, book.getName()));
     }
 
     @Transactional
